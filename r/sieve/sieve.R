1d36f9811cee1c546eb2b7bc0977e376c04789bf
Jonathan Carroll <github@jcarroll.com.au>
2024-11-28 22:24:33 +1030

    regex R solution

--- r/sieve/sieve.R
+++ r/sieve/sieve.R
@@ -2,10 +2,8 @@
 sieve <- function(limit) {
   if (limit == 1) return(NULL)
   s <- vector(length = limit)
-  s[2:limit] <- TRUE
   for (i in 2:limit) {
-    if (!s[i] || 2*i > limit) next
-      s[seq(2*i, limit, i)] <- FALSE
+    s[i] = length(grep(r"(^.?$|^(..+?)\1+$)", strrep('!', i))) == 0
   }
   which(s)
 }
